2ca7844591886202adc240b156fd430f79fd8c24
Satyam Prasad Tiwari <satyamprasadtiwari@gmail.com>
2022-03-08 12:36:00 +0530

    updating with precision and recall for every class

--- Evaluation Metrics/precision_recall_for_each_clasess.py
+++ Evaluation Metrics/precision_recall_for_each_clasess.py
@@ -1,5 +1,5 @@
 
-## TODO: Precision and Recall for Individual classes same as classification_report
+# Precision and Recall for Individual classes same as classification_report
 import numpy as np
 from sklearn.metrics import classification_report
 
@@ -11,6 +11,13 @@ def true_positive(y_true,y_pred, class_):
             count += 1 
     return count
 
+def true_negative(y_true,y_pred, class_):
+    count = 0
+    for yt, yp in zip(y_true,y_pred):
+        if yt == yp != class_:
+            count += 1 
+    return count    
+
 def false_positive(y_true,y_pred, class_):
     count = 0
     for yt, yp in zip(y_true,y_pred):
@@ -19,6 +26,13 @@ def false_positive(y_true,y_pred, class_):
                 count += 1 
     return count
 
+def false_negative(y_true,y_pred, class_):
+    count = 0
+    for yt, yp in zip(y_true,y_pred):
+        if yp != class_:
+            if yt == class_:
+                count += 1 
+    return count
 
 def precision(y_true, y_pred, class_):
     tp = true_positive(y_true, y_pred, class_)
@@ -26,11 +40,17 @@ def precision(y_true, y_pred, class_):
     return tp/(tp+fp)
 
 
-l1 = [0,1,1,1,0,0,0,1]
-l2 = [0,1,0,1,0,1,0,0]
+def recall(y_true, y_pred, class_):
+    tp = true_positive(y_true, y_pred, class_)
+    fn = false_negative(y_true, y_pred, class_)
+    return tp/(tp+fn)
 
+l1 = [0,1,1,1,0,0,0,1,2,3,2,3]
+l2 = [0,1,0,1,0,1,0,0,2,3,2,2]
+
+print("\tClass\tPrecision\tRecall")
 for class_ in np.unique(l1):
-    print(f"Class {class_} has Precision of: {round(precision(l1,l2,class_),2)}")
+    print(f"\t{class_}\t{round(precision(l1,l2,class_),2)}\t\t{round(recall(l1,l2,class_),2)}")
 
 
 print(classification_report(l1,l2))
